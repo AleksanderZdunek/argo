dc757cee22b66782dccc46d9933647def3dcd9a2
aleksander <aleksander@normandy>
2025-11-14 19:06:58 +0100

    Add DEBUG_EXPR macro to C project template

--- templates/c/main.c
+++ templates/c/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define DEBUG_EXPR(expr) fprintf(stderr, "%s:%d:%s(): %s: 0x%llX\n", __FILE__, __LINE__, __func__, #expr, (unsigned long long)(expr))
+
 int main(int argc, char* argv[])
 {
     printf("Hello, World!\n");
